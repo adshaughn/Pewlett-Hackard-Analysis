eef25769b3ce7ee7dd8e25738f290b9d62a73c1c
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-02-18 15:18:34 -0700

    update

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -46,3 +46,22 @@ from unique_titles
 group by title
 order by count(emp_no) desc;
 
+-- Deliverable 2
+
+-- Mentorship eligibility
+
+select distinct on (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+into mentorship_eligibility	
+from employees as e
+inner join dept_emp as de on e.emp_no = de.emp_no
+inner join titles as t on e.emp_no = t.emp_no
+where de.to_date = '9999-01-01'
+	and e.birth_date between '1965-01-01' and '1965-12-31'
+group by e.emp_no, de.from_date, de.to_date, t.title
+order by emp_no asc;
